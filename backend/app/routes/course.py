63f5b3c3767d1b51f44cb33f5dc943efd785b6dc
SadmanZahin <kazisadman897@gmail.com>
2026-06-18 15:09:46 +0600

    added course all backend codes

--- backend/app/routes/course.py
+++ backend/app/routes/course.py
@@ -13,3 +13,25 @@ router = APIRouter(
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def add_course(course_add:CourseCreate,  db:Session = Depends(get_db)):
     return course_service.create_course(db, course_add)
+
+@router.get("/", response_model = list[CourseResponse], status_code=status.HTTP_201_CREATED)
+def get_courses(db:Session = Depends(get_db)):
+    return course_service.get_all_courses(db)
+
+
+# below path{id} and get_course_by_id function parameter id  name always same
+@router.get("/{id}",response_model= CourseResponse, status_code=status.HTTP_201_CREATED)
+def get_course_by_id(id:int,db:Session = Depends(get_db) ):
+    return course_service.get_specificCourse(db, id)
+
+
+@router.delete("/{cs_id}",
+               status_code=status.HTTP_204_NO_CONTENT)
+def delete_course_by_id(cs_id: int, db: Session = Depends(get_db)):
+    course_service.delete_specific_course(db, cs_id)
+
+
+@router.put("/{course_id}", response_model=CourseResponse)
+def update_course(course_id: int, course_update: CourseCreate, db: Session = Depends(get_db)):
+    
+    return course_service.update_course_by_id(db, course_id, course_update)
--- backend/app/services/course_service.py
+++ backend/app/services/course_service.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.models.course import Course # Department is the model database class
 from app.schemas.course import CourseCreate
-
+from app.models.department import Department
 def get_course_or_404(db:Session, course_id :int):
 
     course = db.query(Course).filter(Course.id == course_id).first()
@@ -39,5 +39,37 @@ def create_course(db:Session, course_data: CourseCreate):
     db.refresh(new_course)
     return new_course
 
+def get_all_courses(db:Session):
+    return db.query(Course).all()
 
 
+def get_specificCourse(db:Session, Course_Id:int):
+    course = get_course_or_404(db, Course_Id)
+    return course
+
+def delete_specific_course(db:Session, course_id:int):
+    course = get_course_or_404(db, course_id)
+    db.delete(course)
+    db.commit()
+    return {"message": "Course deleted successfully"}
+    
+
+def update_course_by_id(db:Session, course_id: int, course_update: CourseCreate):
+    #check the course is exist in database or not
+    course  = get_course_or_404(db, course_id)
+
+    #validate foreignkey , check is this department_exist 
+    department = db.query(Department).filter(Department.department_id == course_update.department_id).first()
+    if not department:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Target Department not found. Cannot assign course to a non-existing department."
+        )
+
+    course.title = course_update.title
+    course.code = course_update.code
+    course.department_id = course_update.department_id
+      
+    db.commit()           
+    db.refresh(course)   
+    return course
